4aa94916c62745df5277537a8534a96501c22bb7
死の金属と数学。 <35822767+satanmyninjas@users.noreply.github.com>
2018-11-27 19:12:51 -0500

    Add spaces and periods, n00b

--- vector.py
+++ vector.py
@@ -4,7 +4,7 @@ import math
 class Vector:
     """
     A simple plane vector class. Supports addition, subtraction, L2 Norm,
-    plane-polar angle, and scalar product
+    plane-polar angle, and scalar product.
     """
 
     def __init__(self, x=0.0, y=0.0):
@@ -21,24 +21,24 @@ class Vector:
         return not self == other
 
     def __abs__(self):
-        """ Returns L2 (Euclidean) Norm """
+        """ Returns L2 (Euclidean) Norm. """
         return math.sqrt(self.x ** 2 + self.y ** 2)
 
     def __bool__(self):
         return bool(abs(self))
 
     def __add__(self, other):
-        """ Returns component-wise sum as a Vector """
+        """ Returns component-wise sum as a vector. """
         sum_x = self.x + other.x
         sum_y = self.y + other.y
         return Vector(sum_x, sum_y)
 
     def __mul__(self, other):
-        """ Returns dot product """
-        return self.x*other.x + self.y*other.y
+        """ Returns dot product. """
+        return self.x * other.x + self.y * other.y
 
     def __rmul__(self, other):
-        """ Supports (left) scalar multiplication """
+        """ Supports (left) scalar multiplication. """
         return Vector(self.x * other, self.y * other)
 
     def __neg__(self):
@@ -46,11 +46,11 @@ class Vector:
         return Vector(-self.x, -self.y)
 
     def __sub__(self, other):
-        """ Returns component-wise difference as a Vector """
+        """ Returns component-wise difference as a vector. """
         return self + -other
 
     def pol_angle(self, degrees=True):
-        """ Returns the plane-polar angle in degrees unless degrees=False """
+        """ Returns the plane-polar angle in degrees unless degrees=False. """
         a = math.atan2(self.y, self.x)
         if degrees:
             return math.degrees(a)
@@ -59,8 +59,8 @@ class Vector:
 
 
 def angle(a: Vector, b: Vector, degrees=True) -> float:
-    """ Returns the angle between two Vectors in degrees unless degrees=False """
-    a = math.acos((a*b)/(abs(a)*abs(b)))
+    """ Returns the angle between two Vectors in degrees unless degrees=False. """
+    a = math.acos((a * b)/(abs(a) * abs(b)))
     if degrees:
         return math.degrees(a)
     else:
