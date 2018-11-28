e8e529a4619fc7368464d2d49037ff19b1c66d51
死の金属と数学。 <35822767+satanmyninjas@users.noreply.github.com>
2018-11-28 14:19:21 -0500

    Add capitalization + periods for readability in docstrings

--- vector.py
+++ vector.py
@@ -4,7 +4,7 @@ import math
 class Vector:
     """
     A simple n-dimensional vector class. Supports addition, subtraction, L2 Norm,
-    dot product and scalar multiplication
+    dot product and scalar multiplication.
     """
 
     def __init__(self, *components):
@@ -17,14 +17,14 @@ class Vector:
         return self.components == other.components
 
     def __abs__(self):
-        """ Returns L2 (Euclidean) Norm """
+        """ Returns L2 (Euclidean) Norm. """
         return math.sqrt(sum([c**2 for c in self.components]))
 
     def __bool__(self):
         return bool(abs(self))
 
     def __add__(self, other):
-        """ Returns component-wise sum as a Vector """
+        """ Returns component-wise sum as a vector. """
         # TODO: Rewrite to accommodate n-dimensional vectors
         # HW (11/28)
         sum_x = self.x + other.x
@@ -32,26 +32,26 @@ class Vector:
         return Vector(sum_x, sum_y)
 
     def __mul__(self, other):
-        """ Returns dot product """
+        """ Returns dot product. """
         # TODO: Rewrite to accomodate n-dimensional vectors
         # HW (11/28)
         return self.x*other.x + self.y*other.y
 
     def __rmul__(self, other):
-        """ Supports (left) scalar multiplication """
+        """ Supports (left) scalar multiplication. """
         return Vector(self.x * other, self.y * other)
 
     def __neg__(self):
-        """ Unary minus support """
+        """ Unary minus support. """
         return Vector(-self.x, -self.y)
 
     def __sub__(self, other):
-        """ Returns component-wise difference as a Vector """
+        """ Returns component-wise difference as a vector. """
         return self + -other
 
 
 def angle(a: Vector, b: Vector, degrees=True) -> float:
-    """ Returns the angle between two Vectors in degrees unless degrees=False """
+    """ Returns the angle between two vectors in degrees unless degrees=False. """
     a = math.acos((a*b)/(abs(a)*abs(b)))
     if degrees:
         return math.degrees(a)
